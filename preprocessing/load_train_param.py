70f16e7d0767a70bd82d83611a166063351b668a
imenba <imen.benamor@supcom.tn>
2022-11-02 15:21:18 +0100

    delete preprocessing.py

--- preprocessing/load_train_param.py
+++ preprocessing/load_train_param.py
@@ -2,7 +2,7 @@
 #  Copyright (c) 2022. Imen Ben Amor
 # ==============================================================================
 
-def load_filter_soft(min_typ,typ_path,dout_path):
+def load_filter_soft(typ_path,dout_path):
     #train typicality from file
     with open (typ_path,"r") as f:
         text=f.readlines()
@@ -10,20 +10,12 @@ def load_filter_soft(min_typ,typ_path,dout_path):
     typ={}
     for t in text:
         typ[t.split(":")[0].strip()]=float(t.split(":")[1].strip())
-    #remove typ=0
-    filtered_typ={}
-    deleted_va={}
-    for va in typ.keys():
-        if typ[va]>min_typ:
-            filtered_typ[va]=typ[va]
-        else:
-            deleted_va[va]=typ[va]
+
     #train dropout from file
     with open (dout_path,"r") as f:
         text=f.readlines()
         f.close()
     dropout={}
     for t in text:
-        if t.split(":")[0].strip() in filtered_typ.keys():
-            dropout[t.split(":")[0].strip()]=float((t.split(":")[1]).strip())
-    return filtered_typ,dropout,deleted_va
+        dropout[t.split(":")[0].strip()]=float((t.split(":")[1]).strip())
+    return typ,dropout
